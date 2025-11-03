11152a547ba1cf06b58d5d3f2299c3edb4920e8a
Tuan <tuan.kurtulush1234@gmail.com>
2025-11-03 01:17:42 +0100

    Second commit

--- src/main/java/org/example/CarWithLombok.java
+++ src/main/java/org/example/CarWithLombok.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"brand"})
+@ToString(exclude = "quantity")
 public class CarWithLombok {
+    private String model;
+    private String brand;
+    private int year;
+    private int price;
+    private String color;
+    private int quantity;
 }
